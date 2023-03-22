f74de69b1b49b2637652f3966bfb68c277133fe3
schmeb0704 <seanauditor0704@gmail.com>
2023-03-22 12:17:29 +0800

    next gen js features supported in TS. Object and array dest included.

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,19 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', clicked);
-function clicked() {
-    console.log('clicked!');
-}
+const person = {
+    name: 'Sean',
+    age: 27,
+    hobbies: ['coding', 'eating', 'playing'],
+};
+const person2 = {
+    name: 'Cher',
+    age: 24,
+    hobbies: ['reading', 'tarot card reading', 'surfing the web'],
+};
+const copiedPerson = Object.assign({}, person);
+console.log(copiedPerson);
+person.age = 23;
+console.log(person, copiedPerson);
+const { age, name: personName } = copiedPerson;
+const [hobby1, hobby2, hobby3] = person.hobbies;
+console.log(age, personName);
+console.log(hobby1);
